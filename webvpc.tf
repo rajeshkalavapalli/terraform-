555bb2a03dd9602f52aa9151f2ce313594edc941
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-07 21:09:30 +0530

    update

--- webvpc.tf
+++ webvpc.tf
@@ -21,7 +21,7 @@ resource "aws_vpc" "wisteria-web" {
 resource "aws_subnet" "wisteria-db-sub" {
   vpc_id     = aws_vpc.wisteria-web.id
   cidr_block = "10.0.2.0/24"
-  availability_zone  = "us-east-2b"
+  availability_zone  = "us-east-2c"
   map_public_ip_on_launch = "false"
 
   tags = {
