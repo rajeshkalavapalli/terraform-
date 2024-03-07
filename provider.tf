bf2193c6d3fee5a00646cd57f49dc475acd86239
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-07 17:01:39 +0530

    provider added

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "variable access_key"
+  secret_key = "variable secret_key"
+}
---
+++ var.tf
@@ -0,0 +1,10 @@
+variable access_key {
+  type        = string
+  default     = ""
+  description = ""
+}
+variable secret_key {
+  type        = string
+  default     = ""
+  description = "description"
+}
