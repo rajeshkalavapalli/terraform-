27de82b83b87047c79e8ea81c490f50584ff9d4c
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-07 20:52:58 +0530

    subnet added

--- webvpc.tf
+++ webvpc.tf
@@ -6,3 +6,25 @@ resource "aws_vpc" "wisteria-web" {
     Name = "wisteria"
   }
 }
+#web subnet 
+ resource "aws_subnet" "wisteria-web-sub" {
+  vpc_id     = aws_vpc.wisteria-web.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-2b"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "wisteria-web-sub"
+  }
+}
+# database subnet 
+resource "aws_subnet" "wisteria-db-sub" {
+  vpc_id     = aws_vpc.wisteria-web.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone  = "us-east-2b"
+  map_public_ip_on_launch = "false"
+
+  tags = {
+    Name = "wisteria-db-sub"
+  }
+}
