95533cdf0033263249093cd53d370e8c716a2b5d
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-07 18:06:39 +0530

    provider configration done sucess fully

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "us-east-2"
-  access_key = "variable access_key"
-  secret_key = "variable secret_key"
+  region      = "us-east-2"
+  access_key  = var.access_key
+  secret_key  = var.secret_key
 }
