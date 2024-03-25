92681d6d7e6b3b002d3645fd832460985afab6e0
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-25 16:33:44 +0530

    loops and condition added

---
+++ tf_robo/loops/count.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "roboshop_web" {
+  count = 11
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+
+  tags = {
+    Name = var.instance-name[count.index]
+  }
+}
