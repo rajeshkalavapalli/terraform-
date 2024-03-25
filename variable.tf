3cfe6c3f2f708333a9fa266477ac00773af42809
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-25 12:50:47 +0530

    new files created

--- variable.tf
+++ variable.tf
@@ -1,2 +1,20 @@
-variable "access_key" {}
-variable "secret_key" {}
+variable "sg-name" {
+    type = string
+    default = "roboshop-all"
+}
+
+variable "sg-description" {
+  type = string
+  default = "Allow TLS inbound traffic and all outbound traffic"
+}
+
+variable "inbound-from-port" {
+    type = string
+    default = 0
+}
+
+variable "cidr_blocks" {
+    type = list 
+    default = ["0.0.0.0/0"]
+  
+}
